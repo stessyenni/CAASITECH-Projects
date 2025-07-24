b7a0e2232ede6291b2223686ac2854b20fba6802
Stessy Enni <stessyangeck8@gmail.com>
2025-07-24 15:57:59 +0100

    new updates

--- Basic-calculator/main.js
+++ Basic-calculator/main.js
@@ -1,21 +1,10 @@
-function add (a, b) {
-  return a + b
-}
-
-function subtract (a, b) {
-  return a - b
-}
-
-function multiply (a, b) {
-  return a * b
-}
-
-function divide (a, b) {
-  return a / b
-}
-
 function calculator (a, b, operator) {
-  return operator(a, b)
+  let calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+  }
+  return calculator[operator](a, b)
 }
-
-console.log(calculator(34, 86, multiply))
+console.log(calculator(34, 86, 'multiply'))
