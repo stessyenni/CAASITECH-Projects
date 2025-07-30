7f466db1d679c26b1f4a80d535bd87c66a5b9ed5
Stessy Enni <stessyangeck8@gmail.com>
2025-07-30 13:25:26 +0100

    functions updates

--- functions/main.js
+++ functions/main.js
@@ -1,11 +1,12 @@
-function value () {
-  return 27
-}
+// function value () {
+// return value()
+// }
 function whatType (value) {
+  let whatType = value
   if (value % 2 === 0) {
-    console.log(`${value} This is an Even Number`)
+    console.log(`${value} is an Even Number`)
   } else {
-    console.log(`${value} This is an Odd Number`)
+    console.log(`${value} is an Odd Number`)
   }
 }
-console.log(whatType(27))
+console.log(whatType(545))
