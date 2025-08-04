e4cc9645898d732a1c7cb23467fd2aaf7752aab3
Stessy Enni <stessyangeck8@gmail.com>
2025-08-04 16:37:19 +0100

    New Comments

--- arrays-functions/main.js
+++ arrays-functions/main.js
@@ -1,13 +1,15 @@
+// creating a global variable with 2 arrays within an arraytype variable
 const arraytype = [[10, 23, 44], ['Apple', 'Orange', 'Banana']]
 let number = arraytype[0]
 let fruits = arraytype[1]
 
+// creating a function numstrings with the global variable as the function parameter
 function numstrings (arraytype) {
   for (let i = 0; i < arraytype.length; i++) {
     const array = arraytype[i]
     // console.log(`Type: ${typeof array}`)
 
-    // // The number / string action with if/else
+    // // Determine the number and string with if/else displaying in an array form
     if (array === arraytype[0]) {
       console.log(number)
     } else if (array === arraytype[1]) {
