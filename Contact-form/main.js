0bc28ce325771fedcb92adee8ac349e6ba16a465
Stessy Enni <stessyangeck8@gmail.com>
2025-08-11 11:49:17 +0100

    new changes on form

--- Contact-form/main.js
+++ Contact-form/main.js
@@ -1,6 +1,3 @@
-document.addEventListener['DOMContentLoaded'.function() {
-    var contactForm = document.querySelectorAll('form-container');
-
-    //adding functionality to the form's submit button
-    const submitForm = document.form['submitBtn']
-}]
+document.addEventListener('DOMContentLoaded', function () {
+  var contactForm = document.querySelectorAll('form-container')
+})
