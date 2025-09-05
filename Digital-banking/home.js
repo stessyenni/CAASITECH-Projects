c8fd4c65eb110f71b014b6b5f09e2056e5318505
Stessy Enni <stessy.enni@gmail.com>
2025-09-05 13:52:41 +0100

    Active class function

--- Digital-banking/home.js
+++ Digital-banking/home.js
@@ -1,1 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {})
+document.addEventListener('DOMContentLoaded', function () {
+  let navitems = document.querySelectorAll('nav div ul li a')
+
+  navitems.forEach((item) => {
+    item.addEventListener('click', () => {
+      navitems.forEach((item) => {
+        item.classList.remove('active')
+      })
+      item.classList.add('active')
+    })
+  })
+})
