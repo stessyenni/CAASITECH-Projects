0d4c618cfb1318ab7771c7225eb57cf1132bb89d
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-09-10 15:50:40 +0100

    My Portfolio site

---
+++ Portfolio-site/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let navitems = document.querySelectorAll('nav div ul li a')
+  let sideBar = document.querySelectorAll('.sidebar ul li a')
+  let sidebarBtn = document.querySelector('.sideBtn')
+  let sidebar = document.querySelector('.sidebar')
+  let closeBtn = document.querySelector('.sidebar .sideBtn')
+
+  navitems.forEach((item) => {
+    item.addEventListener('click', () => {
+      navitems.forEach((item) => {
+        item.classList.remove('active')
+      })
+      item.classList.add('active')
+    })
+  })
+})
