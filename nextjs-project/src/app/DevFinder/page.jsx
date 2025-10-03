b9d40cf64018e1954c1b5e6a8c91adca689b318e
Stessy Enni <50053328+stessyenni@users.noreply.github.com>
2025-10-03 13:15:24 +0100

    Adding Input props

--- nextjs-project/src/app/DevFinder/page.jsx
+++ nextjs-project/src/app/DevFinder/page.jsx
@@ -1,13 +1,31 @@
+'use client';
 import React from 'react'
 import SearchBar from '@/components/SearchBar'
 import DevHeader from './DevHeader'
 import DevFinder from './DevFinder'
+import { useEffect, useState } from 'react'
 
 const page = () => {
+    const [input, setInput] = useState('');
+    console.log(input);
+
+    const devfetch = async () => {
+        const res = await fetch(`https://api.github.com/users/${input}`);
+        const users = await res.json()
+        console.log(users);
+    }
+
+    useEffect(() => {
+        devfetch()
+    })
+
   return (
       <div className='font-mono rounded-lg p-30 bg-[#141d2f] flex flex-col gap-[20px] items-center'>
           <DevHeader />
-          <SearchBar />
+          <SearchBar
+              input={input}  
+              setInput={setInput}
+        />
           <DevFinder />
     </div>
   )
--- nextjs-project/src/components/SearchBar.jsx
+++ nextjs-project/src/components/SearchBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {IoSearch} from "react-icons/io5";
+//import {useState} from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
     return (
-        <div className='w-[60%] h-[50px] bg-[#1e2a48] rounded-lg flex items-center relative'>
+        <div className='w-[60%] h-[50px] px-10 bg-[#1e2a48] rounded-lg flex items-center relative'>
             <button>
-                <IoSearch className='text-2xl text-blue-700' />
+                <IoSearch className='text-3xl absolute left-1 bottom-2 pl-2 text-blue-700' />
             </button>
-            <input className='w-full text-white' type="text" value='Search Github username' />
+            <input className='w-full focus:outline-none text-white' type="text" placeholder='Search Github username' value={props.input}
+                onChange = {(e) => {props.setInput(e.target.value)}}
+            />
             <input className='w-fit h-[40px] absolute top-[6px] bottom-[6px] right-2 bg-[#0079ff] px-[20px] rounded-lg'
                 type="button" value='Search' />
         </div>
